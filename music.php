252cd3f29481b05ca654732981a0046c48550015
Chris Malley <cmalley@pixelzoom.com>
2015-01-04 14:43:55 -0700

    remove PayPal link for Split Shot

--- music.php
+++ music.php
@@ -22,36 +22,9 @@
                   <tr>
                     <td valign="top"><b class="Typewriter2"><big><big><big>Music</big></big></big></b></td>
                   </tr>
-<tr>
-  <td>
-    <table cellpadding="10">
-        <!-- CD cover -->
-        <tr class="san14-plain">
-          <th rowspan="4"><img src="music/Split-Shot-cover.jpg"/>
-          </th>
-          <td><br><br>From the band now known as "<a href="http://www.purplesquirrelmusic.com" target="_blank">Purple Squirrel</a>"! "Split Shot"is a collection of jazz/rock/funk instrumentals, composed and produced with Bob Schlesinger (keys). Featuring Mark Diamond (bass) and Christian Teele (drums).</td>
-        </tr>
-        <!-- CD sampler -->
-        <tr class="san22"><td class="centered-cell">Listen!&nbsp;&nbsp;<a href="https://soundcloud.com/orangeguitar/split-shot" target="_blank"><img style="width:60px;height:60px;vertical-align:middle" src="music/play.png" border="0"/></a></td>
-        </tr>
-        <!-- PayPal text -->
-        <tr class="san22"><td class="centered-cell">Buy! ($12 PayPal)</td>
-        </tr>
-        <!-- PayPal button -->
-        <tr><td class="centered-cell">
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="53NXNDUWP2EM4">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-</form>
-        </td></tr>
-    </table>
-  </td>
-</tr>
 <tr><td>&nbsp;</td></tr>
 <tr><td>
-<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/69521391&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+<iframe width="100%" height="700" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/69521391&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
 </td></tr>
   <tr>
     <td>
