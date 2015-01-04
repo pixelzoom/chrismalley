c7cdd8774450a785b811635d7035e16bef9dd6fc
Chris Malley <cmalley@pixelzoom.com>
2015-01-04 14:16:30 -0700

    replace mp3 links with embedded soundcloud playlist

--- music.php
+++ music.php
@@ -32,7 +32,7 @@
           <td><br><br>From the band now known as "<a href="http://www.purplesquirrelmusic.com" target="_blank">Purple Squirrel</a>"! "Split Shot"is a collection of jazz/rock/funk instrumentals, composed and produced with Bob Schlesinger (keys). Featuring Mark Diamond (bass) and Christian Teele (drums).</td>
         </tr>
         <!-- CD sampler -->
-        <tr class="san22"><td class="centered-cell">Listen!&nbsp;&nbsp;<a href="music/Split-Shot-CD-sampler.mp3" target="_blank"><img style="width:60px;height:60px;vertical-align:middle" src="music/play.png" border="0"/></a></td>
+        <tr class="san22"><td class="centered-cell">Listen!&nbsp;&nbsp;<a href="https://soundcloud.com/orangeguitar/split-shot" target="_blank"><img style="width:60px;height:60px;vertical-align:middle" src="music/play.png" border="0"/></a></td>
         </tr>
         <!-- PayPal text -->
         <tr class="san22"><td class="centered-cell">Buy! ($12 PayPal)</td>
@@ -50,161 +50,9 @@
   </td>
 </tr>
 <tr><td>&nbsp;</td></tr>
-<tr><td>&nbsp;</td></tr>
-                  <tr>
-                    <td valign="top">
-                      <div align="left" class="Typewriter"> Some short audio clips that demonstrate
-                        my playing...</div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap">&nbsp;</td>
-            <td valign="top" nowrap="nowrap">&nbsp;</td>
-            <td valign="top" nowrap="nowrap">&nbsp;</td>
-          </tr>
-          <tr bgcolor="#F2F2F2" class="san12">
-            <td width="165" valign="top" nowrap="nowrap"><small><big><b>Song</b></big></small><br>
-            </td>
-            <td width="126" valign="top" nowrap="nowrap"><b>Time (Size)</b><br>
-            </td>
-            <td width="387" valign="top" nowrap="nowrap"><b>Description</b><br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Split-Shot-CD-sampler.mp3">CD Sampler</a><br></td>
-            <td valign="top" nowrap="nowrap">3:12 (3.1MB)<br></td>
-            <td valign="top">
-                Sampler of tunes from my new CD, "Split Shot". Composed and produced by me and Bob Schlesinger (keys). Featuring Mark Diamond (bass) and Christian Teele (drums).<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Big_Freeze.mp3">Big Freeze</a><br></td>
-            <td valign="top" nowrap="nowrap">2:29 (2.3MB)<br></td>
-            <td valign="top">
-                6/8 fusion thing that I did while playing around with the Addictive Drums plug in.
-                [Warmoth Strat, Bad Cat Cub, Foxrox Octron]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Pretty_Song.mp3">Pretty Song</a><br></td>
-            <td valign="top" nowrap="nowrap">0:53 (830KB)<br></td>
-            <td valign="top">
-                Mandolin on this track from Rebecca Folsom's "Water On Stone" album.<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Inside_Edge_clip.mp3">Inside Edge</a><br></td>
-            <td valign="top" nowrap="nowrap">1:40 (2MB)<br></td>
-            <td valign="top">
-                Demo for an original jazz-fusion instrumental.
-                [Warmoth Strat, Bad Cat Cub]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Public_Enemy_clip.mp3">Public Enemy</a><br></td>
-            <td valign="top" nowrap="nowrap">1:26 (1.7MB)<br></td>
-            <td valign="top">
-                Original jazz-fusion instrumental. Featuring Bob Story
-                (harmony guitar), Marc Vullo (bass) and Andrea Beccaro (drums). 
-                [Fender Stratocaster, Mesa Boogie MkIIC]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/If_Its_Magic_clip.mp3">If It's Magic</a><br></td>
-            <td valign="top" nowrap="nowrap">0:52 (1MB)<br></td>
-            <td valign="top">
-              Jazz ballad, from Reese's Monkey's album "Bigger
-              Than It Looks". Featuring Kim Reese on vocals. 
-              [Guild Manhattan]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Ive_Always_Thought_Of_You.mp3">I've Always Thought Of You</a><br></td>
-            <td valign="top" nowrap="nowrap">0:59 (900KB)<br></td>
-            <td valign="top">
-              My interpretation of an acoustic piece by the great Tommy Emmanuel.
-              [Lowden S25c]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Song_For_George.mp3">Song For George</a><br></td>
-            <td valign="top" nowrap="nowrap">0:56 (870KB)<br></td>
-            <td valign="top">
-              My interpretation of a solo acoustic guitar by Eric Johnson.
-              [Lowden S25c]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Renman_Blues.mp3">Renman Blues</a><br></td>
-            <td valign="top" nowrap="nowrap">0:58 (910KB)<br></td>
-            <td valign="top">
-            Slow minor blues. Backing track by <a href="http://www.dolphinstreet.com" target="_default">Robert Renman</a>.
-              [Fender Stratocaster, Bad Cat Cub]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Natural_Disaster_clip.mp3">Natural Disaster</a><br></td>
-            <td valign="top" nowrap="nowrap">0:36 (700KB)<br></td>
-            <td valign="top">
-              Funk/pop tune, from Rebecca Folsom's album "Shine".  
-              [Warmoth Strat, Blues Junior]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Slips_Away_clip.mp3">Slips Away</a><br></td>
-            <td valign="top" nowrap="nowrap">1:26 (1.6MB)<br></td>
-            <td valign="top">
-              From Jim Cohn's "Homage" album. 
-              [Warmoth Tele, Bad Cat Cub]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Band_du_Garage_clip.mp3">Band du Garage</a><br></td>
-            <td valign="top" nowrap="nowrap">0:48 (940KB)<br></td>
-            <td valign="top">
-              Something I threw together using Apple's Garage Band recording software. 
-              [Warmoth Strat, Bad Cat Cub]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Scrapple_clip.mp3">Scrapple From The Apple</a><br></td>
-            <td valign="top" nowrap="nowrap">0:40 (780KB)<br></td>
-            <td valign="top">
-              Jazz standard, featuring Mickey D'Onofrio (bass), Jimmy Yozell (drums) and 
-              Ricardo Pe&ntilde;a (piano). 
-              [Warmoth Strat, Blues Junior]<br>
-            </td>
-          </tr>
-          <tr><td>&nbsp;</td></tr>
-          <tr class="san12">
-            <td valign="top" nowrap="nowrap"><a href="music/Lets_Be_Too_Much_clip.mp3">Let's Be Too Much</a><br></td>
-            <td valign="top" nowrap="nowrap">0:46 (902KB)<br></td>
-            <td valign="top">
-              Demo for this pop-rock tune from Rebecca Folsom's "Shine" album.
-              [Warmoth Strat, Bad Cat Cub]<br>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </td>
-  </tr>
+<tr><td>
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/69521391&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"></iframe>
+</td></tr>
   <tr>
     <td>
       <center>
