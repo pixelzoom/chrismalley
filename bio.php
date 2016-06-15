f0845c0aa429d59aac5d0625daf67c71ba222198
Chris Malley <cmalley@pixelzoom.com>
2016-06-15 14:07:09 -0600

    bio page refresh

--- bio.php
+++ bio.php
@@ -41,25 +41,25 @@
               <span class="san12">
               <br>
 Welcome to my website!
-<b>Guitar</b> is my main instrument. I also play violin, mandolin and bass guitar. 
-And <b>I sing</b> (lead and harmonies).
-I'm a <b>versatile</b> player, comfortable with a wide range of styles and sounds.  
-R&amp;B, funk, blues, rock, jazz, country, bluegrass,... I'm your guy.
+Guitar is my main instrument, and I can double on violin, mandolin, bass guitar - anything with strings! 
+I also sing (lead and harmonies).
+I'm a versatile player, comfortable with a wide range of styles and sounds.  
+Jazz, R&amp;B, funk, blues, rock, country, bluegrass,... I'm your guy.
 <br><br>
-I've been playing professionally most of my life, which started in <b>Connecticut</b>. 
+I've been playing professionally most of my life, which started in Connecticut. 
 I began playing the violin at age 8, then
 picked up the guitar at age 13. My early training was in classical violin
 and theory. I developed my guitar chops playing in bands on the East coast.
-After several years of playing the clubs on <b>New York City's</b>
-infamous Bleecker Street, I moved to <b>Los Angeles</b> where I attended 
-<a href="http://www.mi.edu" target="_blank"><b>Musicians Institute</b></a>. 
-After graduating from Musicians Institute with honors, I worked
-around Los Angeles as a studio musician and recording engineer. In 1990, I escaped to <b>Boulder,
-Colorado</b>, where I live now.</span>
+After several years of playing the clubs in New York City, I moved to Los Angeles, 
+where I attended <a href="http://www.mi.edu" target="_blank"><b>Musicians Institute</b></a>. 
+I graduated from Musicians Institute with honors, then worked
+around Los Angeles as a freelance musician and recording engineer. 
+In 1990, I escaped to Boulder, Colorado, where I live now.</span>
 <br><br>
-My main project is <a href="http://www.purplesquirrelmusic.com">Purple Squirrel</a>, an instrumental jazz/funk quartet. I'm the primary composer, and you can find us playing around Boulder/Denver on a regular basis.
+My main project is <a href="http://www.purplesquirrelmusic.com">Purple Squirrel</a>, an instrumental jazz/funk quartet. I'm the primary composer, and I've written over 100 tunes for this project (52 of them in 2014, one per week!) 
+You can catch <a href="http://www.purplesquirrelmusic.com">Purple Squirrel</a> playing around Boulder/Denver on a regular basis. Visit our website at <a href="http://www.purplesquirrelmusic.com">www.purplesquirrelmusic.com</a>.
 <br><br>
-I'm also a busy freelancer, playing with a bunch of different groups - they're listed below,
+In addition to my own projects, I'm also a busy freelancer. Some of the artists that I perform with are listed below,
 and I encourage you to visit their web sites. 
 Visit my <a href="shows.php">Shows</a> page for a list of past and future live shows.
 I've done a lot of studio work (both as a musician and engineer), and some of that work
@@ -71,114 +71,6 @@ Let me know what you think about the site.
 And if you're a musician who's interested in gigging, recording or songwriting, 
 don't hesitate to contact me.
 <br><br>
-<img src="images/pixel.gif" width="100%" height="1"><br>
-<big><big><b><small>Projects</small></b></big></big><br>
-<br>
-Some of the artists that I've worked with...<br>
-<table width="100%" border="0"
- align="center" cellpadding="5" cellspacing="5">
-  <tbody>
-    <tr class="san12">
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.purplesquirrelmusic.com" target="_blank"><img src="bio/purplesquirrel.jpg"><br>Purple Squirrel</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.rebeccafolsom.com" target="_blank"><img src="bio/rebecca.jpg"><br>Rebecca Folsom</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.ninastorey.com" target="_blank"><img src="bio/ninastorey.jpg"><br>Nina Storey</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.lisabellmusic.com" target="_blank"><img src="bio/lisabell.jpg"><br>Lisa Bell</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.lindalewellyn.com" target="_blank"><img src="bio/lewellyn.jpg"><br>Linda Lewellyn</a><br>
-        </center>
-      </td>
-    </tr>
-    <tr class="san12">
-      <td width="20%" valign="top">         
-        <center>
-          <a href="http://www.darrenkramer.com/" target="_blank"><img src="bio/dko.jpg"><br>Darren Kramer</a><br> 
-        </center>
-      </td>
-      <td width="20%" valign="top">          
-        <center>
-          <a href="http://www.funkiphino.com/" target="_blank"><img src="bio/funkiphino.jpg"><br>Funkiphino</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="bio/whitebredfunk.jpg" target="_blank"><img src="bio/whitebredfunk_small.jpg"><br>White Bred Funk</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">           
-        <center>
-          <a href="bio/monkey.jpg" target="_blank"><img src="bio/monkey_small.jpg"> <br>Reese's Monkey</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.poetspath.com/jimcohnhomepage.html" target="_blank"><img src="bio/jimcohn.jpg"><br>Jim Cohn</a><br>
-        </center>
-      </td>
-   </tr>
-   <tr class="san12">
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.bobstory.com" target="_blank"><img src="bio/bobstory.jpg"><br>Bob Story</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.deanoldencottdrummer.com/" target="_blank"><img src="bio/deano.jpg"><br>Dean Oldencott</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.drumoverdubs.com" target="_blank"><img src="bio/mcrae.jpg"><br>Brian McRae</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://profile.myspace.com/index.cfm?fuseaction=user.viewprofile&friendID=162943216" target="_blank"><img src="bio/christianteele.jpg"><br>Christian Teele</a>
-        </center>
-      </td>
-    </tr>
-    <tr class="san12">
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.ericbaines.com" target="_blank"><img src="bio/ericbaines.jpg"><br>Eric Baines</a><br>
-       </center>
-      </td>
-      <td width="20%" valign="top">          
-        <center>
-          <a href="http://www.ericmoonmusic.com" target="_blank"><img src="bio/ericmoon.jpg"><br>Eric Moon</a><br>
-        </center>
-      </td>
-      <td width="20%" valign="top">         
-        <center>
-          <a href="http://www.jenniferlaneonline.com/" target="_blank"><img src="bio/jennifer.jpg"><br>Jennifer Lane</a><br> 
-        </center>
-      </td>
-      <td width="20%" valign="top">
-        <center>
-          <a href="http://www.ricardopena.net" target="_blank"><img src="bio/ricardo.jpg"><br> Ricardo Pe&ntilde;a</a>
-        </center>
-      </td>
-    </tr>
-  </tbody>
-</table>
-<br>
 <img src="images/pixel.gif" width="100%" height="1"><br>
 <big><b>Influences</b></big><br>
 <br>
@@ -200,16 +92,91 @@ BrandX,
 Steely Dan, 
 Toto,
 ...
+<br><br>
+<img src="images/pixel.gif" width="100%" height="1"><br>
+<big><big><b><small>My Projects</small></b></big></big><br>
+<table width="100%" border="0" align="center" cellpadding="5" cellspacing="5">
+  <tbody>
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.purplesquirrelmusic.com" target="_blank"><img src="bio/purplesquirrel.jpg"><br>Purple Squirrel</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.rabblefishmusic.com" target="_blank"><img src="bio/rabblefish.jpg"><br>RabbleFish</a><br></center>
+      </td>
+    </tr>
+  </tbody>
+</table>
+<img src="images/pixel.gif" width="100%" height="1"><br>
+<big><big><b><small>Other Artists</small></b></big></big><br>
 <br>
+Some of the other artists that I've worked with...<br>
+<table width="100%" border="0" align="center" cellpadding="5" cellspacing="5">
+  <tbody>
+
+    <!-- KEYS -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.ericgunnison.com/" target="_blank"><img src="bio/ericgunnison.jpg"><br>Eric Gunnison</a><br></center </td>
+      <td width="20%" valign="top"><center><a href="http://www.robertschlesinger.com" target="_blank"><img src="bio/bobschlesinger.jpg"><br>Bob Schlesinger</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.ericmoonmusic.com" target="_blank"><img src="bio/ericmoon.jpg"><br>Eric Moon</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.revellmusic.com" target="_blank"><img src="bio/adamrevell.jpg"><br>Adam Revell</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.ricardopena.net" target="_blank"><img src="bio/ricardo.jpg"><br> Ricardo Pe&ntilde;a</a></center></td>
+    </tr>
+
+    <!-- BASS -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.markdiamondmusic.com" target="_blank"><img src="bio/mark_diamond.jpg"><br>Mark Diamond</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="https://www.facebook.com/paul.mcdaniel.33821" target="_blank"><img src="bio/paulmcdaniel.jpg"><br>Paul McDaniel</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="https://www.facebook.com/chris.engleman.9" target="_blank"><img src="bio/chrisengleman.jpg"><br>Chris Engleman</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="https://ericthorin.com" target="_blank"><img src="bio/ericthorin.jpg"><br>Eric Thorin</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.ericbaines.com" target="_blank"><img src="bio/ericbaines.jpg"><br>Eric Baines</a><br></center></td>
+    </tr>
+
+    <!-- DRUMS -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.christianteele.com" target="_blank"><img src="bio/christianteele.jpg"><br>Christian Teele</a></center></td>
+      <td width="20%" valign="top"><center><a href="https://www.facebook.com/matthew.amundson.1?fref=ts" target="_blank"><img src="bio/mattamundson.jpg"><br>Matt Amundson</a></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.deanoldencott.com/" target="_blank"><img src="bio/deano.jpg"><br>Dean Oldencott</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.drumoverdubs.com" target="_blank"><img src="bio/mcrae.jpg"><br>Brian McRae</a><br></center></td>
+      <td width="20%" valign="top"><center><img src="bio/matthouston.jpg"><br>Matt Houston</a><br></center></td>
+    </tr>
+
+    <!-- HORNS -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.darrenkramer.com/" target="_blank"><img src="bio/dko.jpg"><br>Darren Kramer</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.bobbyrebz.com" target="_blank"><img src="bio/bobrebholz.jpg"><br>Bob Rebholz</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.facebook.com/rekha.ohal" target="_blank"><img src="bio/rekhaohal.jpg"><br>Rekha Ohal</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.dannavarro.com" target="_blank"><img src="bio/dannavarro.jpg"><br>Dan Navarro</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.martianacres.com" target="_blank"><img src="bio/martianacres.jpg"><br>Martian Acres</a><br></center></td>
+    </tr>
+
+    <!-- SINGERS -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.ninastorey.com" target="_blank"><img src="bio/ninastorey.jpg"><br>Nina Storey</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.rebeccafolsom.com" target="_blank"><img src="bio/rebecca.jpg"><br>Rebecca Folsom</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.lisabellmusic.com" target="_blank"><img src="bio/lisabell.jpg"><br>Lisa Bell</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.lindalewellyn.com" target="_blank"><img src="bio/lewellyn.jpg"><br>Linda Lewellyn</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.jenniferlaneonline.com/" target="_blank"><img src="bio/jennifer.jpg"><br>Jennifer Lane</a><br></center></td>
+    </tr>
+
+    <!-- MISC -->
+    <tr class="san12">
+      <td width="20%" valign="top"><center><a href="http://www.bobstory.com" target="_blank"><img src="bio/bobstory.jpg"><br>Bob Story</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.funkiphino.com/" target="_blank"><img src="bio/funkiphino.jpg"><br>Funkiphino</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="bio/whitebredfunk.jpg" target="_blank"><img src="bio/whitebredfunk_small.jpg"><br>White Bred Funk</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="bio/monkey.jpg" target="_blank"><img src="bio/monkey_small.jpg"> <br>Reese's Monkey</a><br></center></td>
+      <td width="20%" valign="top"><center><a href="http://www.poetspath.com/jimcohnhomepage.html" target="_blank"><img src="bio/jimcohn.jpg"><br>Jim Cohn</a><br></center></td>
+   </tr>
+
+  </tbody>
+</table>
 <br>
 <img src="images/pixel.gif" width="100%" height="1"><br>
 <big><big><b><small>Discography</small></b></big></big><br>
 <br>
-Some of the recordings that I've appeared on...<br>
-       
+Some of the recordings that I've appeared on (&#8224; as leader) ...
+<br>
       <ul>
+         <li>Rebecca Folsom, <i>"Little Medicines / Extraordinary Days"</i> (2016)</li>
          <li>Lisa Bell, <i>"The Italian Project"</i> (2013) - mandolin on 1 track</li>
-         <li>Chris Malley & Bob Schlesinger, <i>"Split Shot"</i> (2012) - guitars, co-writer, engineering, mixing, all tracks</li>
+         <li><b>Chris Malley & Bob Schlesinger, <i>"Split Shot"</i> (2012) - guitars, co-writer, co-producer, engineer, all tracks</b>&nbsp;&#8224</li>
          <li>Rebecca Folsom, <i>"Reunion"</i>, (2013) - guitar on 1 track</li>
          <li>Rebecca Folsom, <i>"Wild Women"</i>, single (2011) - guitar</li>
          <li>Joan Langford, <i>"Dancing In The Kitchen"</i> (2010) - guitar on 10 tracks, mandolin 1 track, vocals 3 tracks</li>
@@ -233,21 +200,22 @@ Some of the recordings that I've appeared on...<br>
          <li>Quiz Dog Productions, <i>"Foo Foo"</i> film soundtrack (2006) - composer</li>
          <li>Quiz Dog Productions, <i>"The Mileage High Club"</i> film soundtrack (2005) - composer</li>
          <li>Various artists, <i>"Boulder Cuv&eacute;e Volume2"</i> compilation (2005) - guitar on 1 track (Rebecca Folsom)</li>
-         <li>Reese's Monkey, <i>"Bigger Than It Looks"</i> (2004) - guitar on all tracks, writer on 1 track</li>
+         <li><b>Reese's Monkey, <i>"Bigger Than It Looks"</i> (2004) - guitar, producer, engineer, all tracks</b>&nbsp;&#8224</li>
          <li>Rebecca Folsom, <i>"Shine"</i> (2004) - guitar on 2 tracks, co-writer on 2 tracks, co-arranger on 2 tracks</li>
          <li>The Mountain (99.5 FM),<i>"Mountain Homegrown Volume 2"</i> compilation (2004) - guitar on 1 track (Rebecca Folsom)</li>
-         <li>The Turkey Jammers, <i>"Turkey Jam 2004"</i> (2004) - guitar on all tracks</li>
+         <li><b>The Turkey Jammers, <i>"Turkey Jam 2004"</i> (2004) - guitar on all tracks</b>&nbsp;&#8224</li>
          <li>Mojo Watson, <i>"Black Beauty"</i> (2004) - background vocals on "You're The One I Love"</li>
-         <li>The Turkey Jammers, <i>"Turkey Jam 2003"</i> (2003) - guitar on all tracks</li>
+         <li><b>The Turkey Jammers, <i>"Turkey Jam 2003"</i> (2003) - guitar on all tracks</b>&nbsp;&#8224</li>
          <li>Rebecca Folsom, <i>"Across The Sky"</i> (2001) - co-arranger on 1 track</li>
          <li>Various artists, <i>"Songs From The Hill"</i> (2001) - guitar on 1 track (Rebecca Folsom)</li>
          <li>Eric Baines, <i>"Different Kinds of People"</i> (2000) - guitar on 2 tracks</li>
          <li>Rebecca Folsom &amp; Milestone, <i>"Many Miles"</i> (1999) - guitar on 6 tracks</li>
-         <li>Reese's Monkey, <i>"Live at Penny Lane"</i> (1997) - guitar on all tracks</li>
+         <li><b>Reese's Monkey, <i>"Live at Penny Lane"</i> (1997) - guitar, producer, engineer, all tracks</b>&nbsp;&#8224</li>
          <li>DP Waldman, <i>"Just Dancers"</i> (1990) - guitar on 1 track</li>
          <li>Private Stock, <i>"Private Stock"</i> (1989) - guitar on all tracks</li>
          <li>The Bugz, <i>"Musicians Institute Student Album"</i> (1988) - guitar on 1 track<br>
          <li>English Eyes, <i>"Stand"</i> EP (1987) - guitar on all tracks</li>
+         </ul>
          </td>
           </tr>
         </tbody>
